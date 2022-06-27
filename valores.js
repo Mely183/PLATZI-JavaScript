c0d5d38e13e42c881d93ac4d0e7d9c22de015b8c
Mely183 <melissa.villarreal183@hotmail.com>
2022-06-27 10:25:12 -0500

    Asignando valores

---
+++ valores.js
@@ -0,0 +1,18 @@
+var nombre = "Oscar";
+
+// Declarar es importante
+
+var edad;
+
+// Inicializar
+
+edad = 30
+  
+var elemento = ["Computadora", "Celular"];
+
+va3r persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona
