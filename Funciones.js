2ad3bd74262bd4feface7c7c0402b25aaba02cef
Mely183 <melissa.villarreal183@hotmail.com>
2022-06-27 10:30:05 -0500

    Ejemplo de funciones

---
+++ Funciones.js
@@ -0,0 +1,13 @@
+//Declarativas
+
+function miFuncion(){
+    return 3;
+}
+
+//Expresión
+
+var miFuncion = function(a,b){
+    return a + b;
+}
+
+miFuncion(); //Aqui es donde ponermos los parametros
